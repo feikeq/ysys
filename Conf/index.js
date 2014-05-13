ce3d82fa05cf0f49fc38e7e8e8b8890219b9cf63
废客泉 <feikeq@163.com>
2014-05-13 14:55:12 +0800

    Create index.js
    配置文件

---
+++ Conf/index.js
@@ -0,0 +1,15 @@
+global.CONFIG = {
+  /* 服务器相关配置可自行添加 */
+  'SITE_NAME':'YSYS云上元素Node.js服务器框架',
+  'COOKIE_DOMIN' : '127.0.0.1', //cookie域
+  'SITE_UPLOADS' : 'uploads/', //上传文件地址
+  /* MySql数据库配置 */
+  MYSQL : {
+      host: 'localhost',
+      port: '3306',
+      user: 'root',
+      password: 'root',
+      database: 'test',
+      dateStrings: false
+  }
+};
